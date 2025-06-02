b744e8966260907b33b955251ff57dcea37ad433
Jan Wójcicki <255423@edu.p.lodz.pl>
2025-06-02 11:19:10 +0200

    initial commit

---
+++ Point.h
@@ -0,0 +1,25 @@
+//
+// Created by Student on 02.06.2025.
+//
+
+#ifndef KLASY_PUNKT_POINT_H
+#define KLASY_PUNKT_POINT_H
+
+#pragma once
+#include <string>
+
+class Point
+{
+private:
+    std::string m_name;
+    double m_x;
+    double m_y;
+public:
+    void setName(const std::string& name);
+    void setXY(double x, double y);
+    double getX();
+    double getY();
+    void show();
+};
+
+#endif //KLASY_PUNKT_POINT_H
