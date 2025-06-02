e12c028eddd70cc88423ca89349715f1c690b8ab
Jan Wójcicki <255423@edu.p.lodz.pl>
2025-06-02 11:41:47 +0200

    Klasy program nr 3

--- Point.h
+++ Point.h
@@ -15,11 +15,16 @@ private:
     double m_x;
     double m_y;
 public:
+    Point();
+    Point(const std::string& name, double x = 0, double y = 0);
+    ~Point();
     void setName(const std::string& name);
     void setXY(double x, double y);
     double getX();
     double getY();
-    void show();
+    void show() const;
+    double distance(const Point& p) const;
+    const Point & distant(const Point& p) const;
 };
 
 #endif //KLASY_PUNKT_POINT_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "Point.h"
+
 using namespace std;
 
 int main()
 {
-    Point p1, p2, p3;
-
-    p1.setName("P1");
-    p1.setXY(10, 20);
-    p2.setXY(30, 40);
+    //Point p1, p2, p3;
+    Point p1("P1", 0, 0);
+    Point p2("P2", 30, 40);
 
     p1.show();
     p2.show();
-    p3.show();
+
+    cout << "Odleglosc pomiedzy punktami wynosi " << p1.distance(p2) << endl;
+    const Point& p = p1.distant(p2);
+    cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
+    p.show();
+
 }
+
